505538dd8e61e01b2ec6739234dd55e48de94c40
AbbasGSC <abbas@gsc.com.tr>
2023-01-31 15:51:06 +0300

    added recently

---
+++ src/views/HomeView.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-app>
+    <NavbarComponent/>
+    <v-container fluid>
+      <SlideComponent/>
+      <CategoryComponent/>
+      <ProductComponent/>
+      <FooterComponent/>
+    </v-container>
+
+  </v-app>
+</template>
+
+<script>
+// @ is an alias to /src
+
+import NavbarComponent from "@/components/NavbarComponent.vue";
+import CategoryComponent from "@/components/CategoryComponent.vue";
+import SlideComponent from "@/components/SlideComponent.vue";
+import ProductComponent from "@/components/ProductComponent.vue";
+import FooterComponent from "@/components/FooterComponent.vue";
+
+export default {
+  name: 'HomeView',
+  components: {
+    FooterComponent,
+    ProductComponent,
+    SlideComponent,
+    CategoryComponent,
+    NavbarComponent,
+  }
+}
+</script>
+<style>
+
+</style>
